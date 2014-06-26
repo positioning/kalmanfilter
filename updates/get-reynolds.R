6a344f00e019e14be6d03bc375310b8d281e3ebc
tagtuna <tagtuna@77a56499-bf6e-355e-e284-15a721bbdb98>
2014-06-26 18:37:37 +0000

    get.reynolds reverted

--- updates/get-reynolds.R
+++ updates/get-reynolds.R
@@ -158,9 +158,7 @@ get.reynolds <- function(track, lonlow, lonhigh, latlow, lathigh,
         xyz <- na.omit(xyz)
         if (removeland) {
 		     xyz <- xyz[which(xyz[,4]==1), -4]
-	    }else{
-		     xyz[which(xyz[,4]==0),3] <- -999
-		}
+	    }
 		if (minus180){
           jy <- order(xyz[,2])
 		  xyz <- xyz[jy,]
