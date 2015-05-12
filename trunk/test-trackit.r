b6bbb564065fb0d0981fe24e20c7256125c17dae
tagtuna@gmail.com <tagtuna@gmail.com@77a56499-bf6e-355e-e284-15a721bbdb98>
2015-05-12 19:59:30 +0000

    sst test should converge

--- trunk/test-trackit.r
+++ trunk/test-trackit.r
@@ -10,7 +10,7 @@ data(driftersst)
 ptsst<-prepit(drifter, sst=driftersst, 
               fix.first=c(360-161.45,22.85,2002,9,10,0,0,0),
               fix.last=c(360-159.87,21.95,2003,5,21,0,0,0), scan=FALSE)
-fitsst<-trackit(ptsst) # Probably can't converge, but whatever!	
+fitsst<-trackit(ptsst, bsst.ph=3, rad.ph=-1)  
 pdf(height=6, width =6, file = 'trackit-exe-test.pdf')
 plot(fit)
 fitmap(fit, ci = T)
