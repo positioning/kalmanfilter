d83dcf1f1167df8dc5f1fe7489e14738cd3d4b4c
tagtuna@gmail.com <tagtuna@gmail.com@77a56499-bf6e-355e-e284-15a721bbdb98>
2015-05-12 19:36:20 +0000

    moved lines for path.packages fixes

--- trunk/test-trackit.r
+++ trunk/test-trackit.r
@@ -1,8 +1,8 @@
 library(trackit)
-source("http://geolocation.googlecode.com/svn/trunk/support/trackit/func_prepit.r")
-source("http://geolocation.googlecode.com/svn/trunk/support/trackit/func_trackit.r")
 data(drifter)
 data(deltat); deltat$JDE = with(deltat,JDE(year,month,day))  # this line is a fix for R 3.0.2.. call me for details if you care that much!
+# source("http://geolocation.googlecode.com/svn/trunk/support/trackit/func_prepit.r")  # for path.packages issues
+# source("http://geolocation.googlecode.com/svn/trunk/support/trackit/func_trackit.r") # for path.packages issues
 prep.track<-prepit(drifter, fix.first=c(360-161.45,22.85,2002,9,10,0,0,0), fix.last=c(360-159.87,21.95,2003,5,21,0,0,0), scan=FALSE)
 fit<-trackit(prep.track)
 sstfolder <- get.sst.from.server(drifter,180,210,10,40)
