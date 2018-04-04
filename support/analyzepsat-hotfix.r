14ad6a7e0ec5c345ca745ae84c603f3be9041252
Tim Lam <tagtuna@gmail.com>
2018-04-04 07:30:54 -0400

    Add one more function to hotfix for analyzepsat

--- support/analyzepsat-hotfix.r
+++ support/analyzepsat-hotfix.r
@@ -5,4 +5,5 @@ source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/make
 source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/plot.btrack.R")
 source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/track2KD.r")
 source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/as.asc.r")
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/CI2shp.R")
 source_url("https://raw.githubusercontent.com/positioning/kalmanfilter/master/support/analyzepsat-old-scripts.r")
