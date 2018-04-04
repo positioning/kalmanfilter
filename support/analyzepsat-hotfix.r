6e3051cc3f7bad949f7ba41a6aa3d3ebf1fcafb3
Tim Lam <tagtuna@gmail.com>
2018-04-04 07:09:15 -0400

    Hotfix for analyzepsat to run the example

---
+++ support/analyzepsat-hotfix.r
@@ -0,0 +1,8 @@
+### Temporary fix for R.3.4.4 
+require(devtools)
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/prepb.R")
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/make.btrack.R")
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/plot.btrack.R")
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/track2KD.r")
+source_url("https://raw.githubusercontent.com/galuardi/analyzepsat/master/R/as.asc.r")
+source_url("https://raw.githubusercontent.com/positioning/kalmanfilter/master/support/analyzepsat-old-scripts.r")
