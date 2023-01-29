0ed35d378f676c666805c5f2937a693a5eabc28c
Tim Lam <tagtuna@gmail.com>
2023-01-29 08:15:09 -0500

    Fixed SST download for NOAA OIsst v2
    Moved over to use THREDDS server and high-res product at 0.25 deg, then upscale to 1 deg or 0.5 deg.

--- support/ukfsst/get-reynolds.R
+++ support/ukfsst/get-reynolds.R
@@ -1,185 +1,162 @@
-get.reynolds <- function(track, lonlow, lonhigh, latlow, lathigh,
-                         folder = tempdir(), removeland = TRUE, minus180 = FALSE, offsetx=0, offset.cutoff=360)
+get.reynolds <- function (track, lonlow, lonhigh, latlow, lathigh, folder = tempdir(), removeland = TRUE, landvalue=NULL, minus180 = FALSE, offsetx = 0, offset.cutoff = 360, res = "low") 
 {
-  # Metadata: http://www.esrl.noaa.gov/psd/data/gridded/data.noaa.oisst.v2.html
-  require(date)
-  require(ncdf4)
-  fmtDate <- function(date) {
+    ### Updated Jan 2023
+	###-------------------------------------------------------------
+	### Switched to THREDDS Data Server
+	### Source: OISST v2 high res @0.25 deg 
+	### then up-scale to "low" res @1.00 deg & "mid" res @0.50
+	### https://psl.noaa.gov/mddb2/makePlot.html?variableID=156648
+	###-------------------------------------------------------------
+    require(date)
+    require(ncdf4)
+    fmtDate <- function(date) {
         x <- date.mdy(date)
         paste(x$year, formatC(x$month, digits = 1, flag = "0", 
             format = "d"), formatC(x$day, digits = 1, flag = "0", 
             format = "d"), sep = "-")
     }
-  fmtDay <- function(day) {
+    fmtDay <- function(day) {
         formatC(day, digits = 2, flag = "0", format = "d")
     }
-  testdir <- file.info(folder)$isdir
-  if (is.na(testdir)) {
+    testdir <- file.info(folder)$isdir
+    if (is.na(testdir)) {
         dir.create(folder)
     }
-  else {
+    else {
         if (!testdir) 
             stop("The folder name supplied is in fact a filename")
-  }
-  unlink(paste(folder, "/*", sep=""), F)
-  sstfolder <- paste(folder, "sst_files", sep="/")
-  testdir <- file.info(sstfolder)$isdir
-  if (is.na(testdir)) {
+    }
+    unlink(paste(folder, "/*", sep = ""), F)
+    sstfolder <- paste(folder, "sst_files", sep = "/")
+    testdir <- file.info(sstfolder)$isdir
+    if (is.na(testdir)) {
         dir.create(sstfolder)
-  }
-  else {
+    }
+    else {
         if (!testdir) 
             stop("The folder name supplied is in fact a filename")
-  }
-  unlink(paste(sstfolder, "/*", sep=""), F)
-  if (is.data.frame(track)) track <- list(track)
-  minDate <- min(unlist(lapply(track, function(x) mdy.date(x[1, 
+    }
+    unlink(paste(sstfolder, "/*", sep = ""), F)
+    if (is.data.frame(track)) 
+        track <- list(track)
+    minDate <- min(unlist(lapply(track, function(x) mdy.date(x[1, 
         2], x[1, 3], x[1, 1]))))
-  maxDate <- max(unlist(lapply(track, function(x) mdy.date(x[nrow(x), 
+    maxDate <- max(unlist(lapply(track, function(x) mdy.date(x[nrow(x), 
         2], x[nrow(x), 3], x[nrow(x), 1]))))
-  latlow <- ifelse(latlow < -89.5, -89.5, trunc(latlow)-0.5)
-  lathigh <- ifelse(lathigh > 89.5, 89.5, trunc(lathigh)+0.5)
-  lonlow <- ifelse(lonlow < 0, trunc(lonlow)+360-0.5, trunc(lonlow)-0.5)
-  lonhigh <- ifelse(lonhigh < 0, trunc(lonhigh)+360+0.5, trunc(lonhigh)+0.5)
-  #
-  # Download and subset land mask
-  link <- "ftp://ftp.cdc.noaa.gov/Datasets/noaa.oisst.v2/lsmask.nc"
-  fname = paste(folder, "landmask.nc", sep = "/")
-  download.file(link, fname, mode="wb")
-  nc <- nc_open(fname)
-  land <- ncvar_get(nc, varid="mask")
-  land <- land[lonlow:lonhigh,((lathigh-90)*-1):((latlow-90)*-1)]
-  nc_close(nc)
-  #
-  # Get dataset ID and find out the end date and file dates of the imagery series
-  link <- "http://www.esrl.noaa.gov/psd/cgi-bin/db_search/DBSearch.pl?Dataset=NOAA+Optimum+Interpolation+(OI)+SST+V2&Variable=Sea+Surface+Temperature"
-  fname = paste(folder, "search.html", sep = "/")
-  download.file(link, fname, mode="wb")
-  dfile <- readLines(fname)
-  unlink(fname)
-  idstring <- dfile[grep("sst.wkmean.1990-present.nc", dfile)-3]
-  idstring <- unlist(strsplit(idstring, "&DB_"))
-  idstring <- idstring[grep("tid", idstring)]
-  idstring <- sub("tid=", "", idstring)
-  dstring <- dfile[grep("sst.wkmean.1990-present.nc", dfile)-1]
-  dstring <- gsub("</td>", "", dstring)
-  dstring <- gsub("<td>", "", dstring)
-  dstring <- as.Date(unlist(strsplit(dstring, " ")), "%Y/%m/%d")
-  enddate <- as.numeric(unlist(strsplit(as.character(dstring[2]), "-")))
-  enddate <- mdy.date(enddate[2],enddate[3],enddate[1])
-  filedates <- seq(dstring[1], dstring[2], 7)
-  filedates <- as.date(as.numeric(filedates)+ 3653)
-  if (minDate >= enddate)
-      stop("Start date of your track is beyond that of the available SST images.")
-  date1 <- which(minDate >= filedates)
-  date1 <- ifelse(length(date1)==0, filedates[1], max(date1))
-  date1 <- filedates[ifelse(date1==1, date1, date1 - 1)]
-  date2 <- which(maxDate <= filedates)
-  date2 <- ifelse(length(date2)==0, length(filedates), min(date2))
-  date2 <- filedates[ifelse(date2==length(filedates), date2, date2 + 1)]
-  cat(paste("Contacting server for SST images within this date range: ", date1, " - ", date2, "\n\n" , sep=""))
-  #
-  # Perform the subset and download netcdf file from FTP
-  latlow <- ifelse(latlow > 0, paste(latlow, "N", sep=""), paste(latlow*-1, "S", sep=""))
-  lathigh <- ifelse(lathigh > 0, paste(lathigh, "N", sep=""), paste(lathigh*-1, "S", sep=""))
-  lonlow <- paste(lonlow, "E", sep="")
-  lonhigh <- paste(lonhigh, "E", sep="")
-  link <- "http://www.esrl.noaa.gov/psd/cgi-bin/GrADS.pl?dataset=NOAA+Optimum+Interpolation+%28OI%29+SST+V2&DB_did=62&file=%2FDatasets%2Fnoaa.oisst.v2%2Fsst.wkmean.1990-present.nc&variable=sst&DB_vid=1296&DB_tid=DATAID&units=degC&longstat=Mean&DB_statistic=Mean&stat=&lat-begin=LATLOW&lat-end=LATHIGH&lon-begin=LONLOW&lon-end=LONHIGH&dim0=time&year_begin=YEARLOW&mon_begin=MONTHLOW&day_begin=DAYLOW&year_end=YEARHIGH&mon_end=MONTHHIGH&day_end=DAYHIGH&X=lon&Y=lat&output=file&bckgrnd=black&use_color=on&cint=&range1=&range2=&scale=100&submit=Create+Plot+or+Subset+of+Data"
-  link <- sub("DATAID", idstring, link)
-  opt <- sub("LATLOW", latlow, link)
-  opt <- sub("LATHIGH", lathigh, opt)
-  opt <- sub("LONLOW", lonlow, opt)
-  opt <- sub("LONHIGH", lonhigh, opt)
-  opt <- sub("YEARLOW", format(as.Date(date1), "%Y"), opt)
-  opt <- sub("MONTHLOW", format(as.Date(date1), "%b"), opt)
-  opt <- sub("DAYLOW", as.numeric(format(as.Date(date1), "%d")), opt)
-  opt <- sub("YEARHIGH", format(as.Date(date2), "%Y"), opt)
-  opt <- sub("MONTHHIGH", format(as.Date(date2), "%b"), opt)
-  opt <- sub("DAYHIGH", as.numeric(format(as.Date(date2), "%d")), opt)
-  fname = paste(folder, "request.html", sep = "/")
-  download.file(opt, fname, mode="wb")
-  dfile <- readLines(fname)
-  unlink(fname)
-  dstring <- dfile[grep("ftp://ftp.cdc.noaa.gov/", dfile)]
-  flink <- unlist(strsplit(unlist(strsplit(dstring, "a href="))[2], ">"))[1]
-  fname <- paste(folder, "oisst.nc", sep = "/")
-  download.file(flink, fname, mode="wb")
-  cat(paste(rep("=", options()$width), collapse = ""), "\n\n")
-  cat(paste("SST data are downloaded as a netcdf file from \n\n", flink, "\n\n" , sep=""))
-  #
-  # Add land mask to oisst.nc
-  nc <- nc_open(fname, write=T)
-  xdim <- nc$dim[['lon']]
-  ydim <- nc$dim[['lat']]
-  varz = ncvar_def("land","flag", list(xdim,ydim), 32767, 
-          longname="Land mask for SST values (1=ocean, 0=land)")
-  nc <- ncvar_add(nc, varz)
-  ncvar_put(nc, "land", land)
-  nc_sync(nc)
-  nc_close(nc)
-  land <- t(land) # the dimensions are flipped
-  #
-  # Extract each day of data as xyz
-  nc <- nc_open(fname)
-  lon <- ncvar_get(nc, varid="lon")
-  lat <- ncvar_get(nc, varid="lat")
-  dates <- as.Date("1800-01-01") + ncvar_get(nc, varid="time")
-  every.day <- 7
-  vv      <- nc$var[[1]]
-  varsize <- vv$varsize
-  ndims   <- vv$ndims
-  nt      <- varsize[ndims]  # Remember timelike dim is always the LAST dimension!
-  for (i in 1:nt){
-        # Initialize start and count to read one timestep of the variable.
-        start <- rep(1,ndims)   # begin with start=(1,1,1,...,1)
-        start[ndims] <- i       # change to start=(1,1,1,...,i) to read timestep i
-        count <- varsize        # begin w/count=(nx,ny,nz,...,nt), reads entire var
-        count[ndims] <- 1       # change to count=(nx,ny,nz,...,1) to read 1 tstep
-        sst <- round(t(ncvar_get(nc, vv, start=start, count=count )),2)
-        # Prepare an individual xyz file
-        xyz <- rbind(rep(NA, 4))
-        d <- mdy.date(as.numeric(format(dates[i], "%m")),
-                      as.numeric(format(dates[i], "%d")),
-                      as.numeric(format(dates[i], "%Y")))
-        # !! The date constructs for the filename are different - the first date is the image date
-        # !! Unlike old code that centers the image date in between d1 and d2 (+/- days to position it)
-	    y1 <- date.mdy(d)$year
+    latlow <- ifelse(latlow < -90, -90, trunc(latlow))
+    lathigh <- ifelse(lathigh > 90, 90, trunc(lathigh))
+    lonlow <- ifelse(lonlow < 0, trunc(lonlow) + 360, trunc(lonlow))
+    lonhigh <- ifelse(lonhigh < 0, trunc(lonhigh) + 360, trunc(lonhigh))
+	### THREDDS catalogue to work out the end date
+	link <- "https://psl.noaa.gov/thredds/ncml/Datasets/noaa.oisst.v2.highres/sst.week.mean.nc?catalog=http://psl.noaa.gov/thredds/catalog/Datasets/noaa.oisst.v2.highres/catalog.html&dataset=Datasets/noaa.oisst.v2.highres/sst.week.mean.ncc"
+    fname = paste(folder, "search.html", sep = "/")
+    download.file(link, fname, mode = "wb")
+    dfile <- readLines(fname)
+    unlink(fname)
+	cfstring <- '    <attribute name="time_coverage_end" value="'
+	dstring <- gsub(cfstring, '', dfile[grep(cfstring,dfile)])
+	enddate <- mdy.date(as.numeric(substring(dstring,6,7)),
+						as.numeric(substring(dstring,9,10)),
+						as.numeric(substring(dstring,1,4)))
+	### Extend weekly time steps by 10000					
+	dbuffer <- 7*10000
+    filedates <- as.Date("1800-01-01") + seq(66357,81463+dbuffer,7)  
+    filedates <- as.date(as.numeric(filedates) + 3653)
+    if (minDate >= enddate) 
+        stop("Start date of your track is beyond that of the available SST images.")
+    date1 <- which(minDate >= filedates)
+    date1 <- ifelse(length(date1) == 0, filedates[1], max(date1))
+    date1 <- filedates[ifelse(date1 == 1, date1, date1 - 1)]
+    date2 <- which(maxDate <= filedates)
+    date2 <- ifelse(length(date2) == 0, length(filedates), min(date2))
+    date2 <- filedates[ifelse(date2 == length(filedates), date2, 
+        date2 + 1)]
+    cat(paste("Contacting server for SST images within this date range: ", 
+        date1, " - ", date2, "\n\n", sep = ""))
+	### Dataset link
+	link <- 
+	'http://psl.noaa.gov/thredds/ncss/grid/Datasets/noaa.oisst.v2.highres/sst.week.mean.nc'
+    link <- paste0(link,"?var=sst&north=LATHIGH&west=LONLOW&east=LONHIGH&south=LATLOW&horizStride=1&time_start=DATELOWT00:00:00Z&time_end=DATEHIGHT00:00:00Z&&&accept=netcdf3")
+	### THREDDS accepts lon in 0-360, so just need to handle a special case of (0,360)
+	if (lonlow == 0 & lonhigh == 360) { lonlow = 0.001; lonhigh = 359.999}
+	### Piece together the THREDDS subsetting link	
+	opt <- sub("LATLOW", latlow, link)
+    opt <- sub("LATHIGH", lathigh, opt)
+    opt <- sub("LONLOW", lonlow, opt)
+    opt <- sub("LONHIGH", lonhigh, opt)
+    opt <- sub("DATELOW", format(as.Date(date1), "%Y-%m-%d"), opt)
+    opt <- sub("DATEHIGH", format(as.Date(date2), "%Y-%m-%d"), opt)
+    fname <- paste(folder, "oisst.nc", sep = "/")
+    download.file(opt, fname, mode = "wb")
+    cat(paste(rep("=", options()$width), collapse = ""), "\n\n")
+    cat(paste("SST data are downloaded as a netcdf file from \n\n", 
+        opt, "\n\n", sep = ""))
+	### Extract individual layers from netcdf file	
+    nc <- nc_open(fname)
+    lon <- ncvar_get(nc, varid = "lon")
+    lat <- ncvar_get(nc, varid = "lat")
+    dates <- as.Date("1800-01-01") + ncvar_get(nc, varid = "time")
+    every.day <- 7
+    vv <- nc$var[[1]]
+    varsize <- vv$varsize
+    ndims <- vv$ndims
+    nt <- varsize[ndims]
+    for (i in 1:nt) {
+        start <- rep(1, ndims)
+        start[ndims] <- i
+        count <- varsize
+        count[ndims] <- 1
+        sst <- round(t(ncvar_get(nc, vv, start = start, count = count)), 2)
+		# Add dummy header row
+        xyz <- rbind(rep(NA,3))
+        d <- mdy.date(as.numeric(format(dates[i], "%m")), as.numeric(format(dates[i], 
+            "%d")), as.numeric(format(dates[i], "%Y")))
+        y1 <- date.mdy(d)$year
         d1 <- d - mdy.date(month = 1, day = 1, year = y1) + 1
         y2 <- date.mdy(d + every.day - 1)$year
-        d2 <- (d + every.day -1) - mdy.date(month = 1, 
-               day = 1, year = y2) + 1
-        filename <- paste("RS", y1, fmtDay(d1), 
-                    "_", y2, fmtDay(d2), "_", "sst", ".xyz", sep = "")
+        d2 <- (d + every.day - 1) - mdy.date(month = 1, day = 1, 
+            year = y2) + 1
+        filename <- paste("RS", y1, fmtDay(d1), "_", y2, fmtDay(d2), 
+            "_", "sst", ".xyz", sep = "")
         dest <- paste(sstfolder, filename, sep = "/")
         for (j in 1:length(lon)) {
-		     m180 <- ifelse(lon[j]>180, lon[j]-360, lon[j]) + offsetx
-             xyz <- rbind(xyz, cbind(lat, ifelse(minus180, m180, lon[j]), sst[,j], land[,j]))
+            m180 <- ifelse(lon[j] > 180, lon[j] - 360, lon[j]) + offsetx
+            xyz <- rbind(xyz, cbind(lat, ifelse(minus180, m180, 
+                lon[j]), sst[, j])) 			  
         }
-        xyz <- na.omit(xyz)
-        if (removeland) {
-		     xyz <- xyz[which(xyz[,4]==1), -4]
-	    } else {
-		     xyz <- xyz[, -4]
+		# Remove dummy header
+		xyz <- xyz[-1,]
+		xyz <- as.data.frame(xyz); names(xyz) <- c("y","x","v")
+		### Upscale to either 1.0 or 0.5 deg
+        if (res != "high"){
+		  deg = ifelse(res == "low", 1, 0.5)
+		  xyz$ay <- xyz[,1] %/% deg * deg
+		  xyz$ax <- xyz[,2] %/% deg * deg
+		  axyz <- with(xyz, aggregate(x = v, by = list(ay,ax), FUN = "mean", na.rm=T))
+		  axyz[,3] <- round(axyz[,3],2)
+		  xyz <- axyz
 		}
-		if (minus180){
-          jy <- order(xyz[,2])
-		  xyz <- xyz[jy,]
-	      jy <- which(xyz[,2]> offset.cutoff)[1]-1
-		  xyz <- xyz[1:jy,]
+		### Flag land values, which is read in as NA
+		idx <- which(is.na(xyz[,3])==T)
+        if (removeland){xyz <- na.omit(xyz)} 
+		else {
+			# Assign land values with a user-specified number
+			if (!is.null(landvalue)) {xyz[idx,3] <- landvalue}
 		}
-        write.table(xyz, file = dest, 
-                    quote = FALSE, row.names = FALSE, col.names = FALSE)
-  }
-  nc_close(nc)
-  cat("And repackaged to", length(dir(sstfolder)), "xyz files in:\n\n  ", sstfolder, "\n\n")
-  cat(paste(rep("=", options()$width), collapse = ""), "\n\n")
-  .sstFileVector <<- paste(sstfolder, dir(sstfolder), sep = "/")
-  return(sstfolder)
-}  
-
-# Not run
-# setwd("C:\\Documents and Settings\\Valued Customer\\Desktop\\R\\getsst")
-# ##source("get-reynolds.R")
-# pts <- read.csv("points.csv")
-# sst.path <- get.reynolds(pts,200,250,10,40, "c:/temp")
-
+        ### Handling tracks crossing from 360 to 0 longitude and vice versa		
+        if (minus180) {
+            jy <- order(xyz[, 2])
+            xyz <- xyz[jy, ]
+            jy <- which(xyz[, 2] > offset.cutoff)[1] - 1
+            xyz <- xyz[1:jy, ]
+        }
+        write.table(xyz, file = dest, quote = FALSE, row.names = FALSE, 
+            col.names = FALSE)
+    }
+    nc_close(nc)
+    cat("And repackaged to", length(dir(sstfolder)), "xyz files in:\n\n  ", 
+        sstfolder, "\n\n")
+    cat(paste(rep("=", options()$width), collapse = ""), "\n\n")
+    .sstFileVector <<- paste(sstfolder, dir(sstfolder), sep = "/")
+    return(sstfolder)
+}
